62160afbf961ad316625ee436860374c11ee49e7
TKSJERO <jeronimozuluagamunoz@gmail.com>
2024-09-14 07:25:58 -0500

    Create TKSJERO.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/TKSJERO.py
@@ -0,0 +1,32 @@
+# https://www.python.org/
+
+# Comentario de una linea
+
+"""
+Esto es
+un comentario
+de varias lineas
+"""
+
+'''
+Esto es
+un comentario
+de varias lineas
+'''
+
+my_variable = "Esta es mi variable"
+my_variable = "Este es el nuevo valor de mi variable"
+
+# Python no tiene variables pero si ponemos l avariable en mayusculas se puede tomar como una constante
+MY_CONSTANT = "Mi constante" # por convencion
+
+# Datos primitivos
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_str = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+# Texto por consola
+print("¡Hola, Python")
